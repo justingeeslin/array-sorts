a9befddbd289965ba81f39952e56b4ae2d9fb66c
Justin Geeslin <justin@geesl.in>
2018-07-12 21:52:20 -0500

    Fixing a horrible bug with my tests. Terrible inaccuracy. Always passes. such a simple change, but a big result

--- tests/index.js
+++ tests/index.js
@@ -9,7 +9,7 @@ describe('TreeSort', function() {
     var theArray = [2,4,6,9,2,4];
 
     // Array sorted natively; remove additional properties.
-    var sortedArray = Object.values(theArray.sort());
+    var sortedArray = Object.values(theArray).sort();
 
     expect(theArray.treeSort()).toEqual(sortedArray);
     console.log('Tree Sort cost:', cost);
